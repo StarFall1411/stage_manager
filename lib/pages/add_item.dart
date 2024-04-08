8bd13c566b18dea77f2866526fe7700cae089f72
Alissa <alissagoff1411@gmail.com>
2024-04-08 11:15:44 -0600

    WIP Added old camera code

--- lib/pages/add_item.dart
+++ lib/pages/add_item.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:stage_manager/models/inventory_item_model.dart';
 import 'package:stage_manager/isar_service.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../models/tag_model.dart';
 
@@ -22,6 +26,8 @@ class _AddItemPageState extends State<AddItemPage> {
   IsarService isar = IsarService();
 
   List<Tag> selectedTags = [];
+  final picker = ImagePicker();
+  String _imagePath = 'assets/default.png';
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +58,15 @@ class _AddItemPageState extends State<AddItemPage> {
             const SizedBox(
               height: 10.0,
             ),
+            const Text('Item Image'),
+            GestureDetector(
+              onTap: ()async{
+                await _showOptions();
+              },
+              child: Image.asset(_imagePath,height: 225.0,width: 225.0,),
+            ),
+            //TODO put a button to default to the default photo
+            //TODO put something to help the user to know to tap to take a new photo
             SizedBox(
               width: screenWidth - 30.0, //TODO decide on width
               child: TextField(
@@ -162,4 +177,60 @@ class _AddItemPageState extends State<AddItemPage> {
                 DataCell(Text(tag.name)),
               ]))
       .toList();
+
+  Future _showOptions() async {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        actions: [
+          CupertinoActionSheetAction(
+            child: Text('Photo Gallery'),
+            onPressed: () {
+              // close the options modal
+              Navigator.of(context).pop();
+              // get image from gallery
+              _getImageFromGallery();
+            },
+          ),
+          CupertinoActionSheetAction(
+            child: Text('Camera'),
+            onPressed: () {
+              // close the options modal
+              Navigator.of(context).pop();
+              // get image from camera
+              _getImageFromCamera();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future _getImageFromGallery() async {
+    final pickedImage =
+    await picker.pickImage(source: ImageSource.gallery); //getImage?
+
+    setState(() async {
+      if (pickedImage != null) {
+        final Directory directory = await getApplicationDocumentsDirectory();
+        final String path = directory.path;
+        final File createdFile = File(pickedImage.path);
+        final File newImage = await createdFile.copy('$path/image1.png');
+        _imagePath = newImage.path;
+      }
+    });
+  }
+
+  Future _getImageFromCamera() async {
+    final pickedImage =
+    await picker.pickImage(source: ImageSource.camera); //getImage?
+
+    setState(() {
+      if (pickedImage != null) {
+        _imagePath = pickedImage.path;
+      }
+    });
+  }
 }
+
+
