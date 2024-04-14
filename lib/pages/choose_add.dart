c9fa0db5cd6f045bd4f90c15c9af387e08357ed3
Alissa <alissagoff1411@gmail.com>
2024-04-14 11:38:08 -0600

    Finished choose add page

--- lib/pages/choose_add.dart
+++ lib/pages/choose_add.dart
@@ -16,99 +16,120 @@ class ChooseAddPage extends StatefulWidget {
 class _ChooseAddPageState extends State<ChooseAddPage> {
   @override
   Widget build(BuildContext context) {
-    IsarService isar = IsarService();
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         centerTitle: true,
-        title: const Text('Inventory'),
+        title: const Text('Choose Item Type to Add'),
       ),
-      body: Column(
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Center(
+      child: Column(
         children: [
           const SizedBox(
             height: 10.0,
           ),
-          Card(
-            child: TextButton(
-                onPressed: () async{
-                  List<Tag> tags = [];
-                  await isar.getAllTags().then((value) {
-                    setState(() {
-                      tags = value;
-                    });
-                  });
-
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AddItemPage(
-                                addItemType: ItemType.prop,
-                            tags: tags,
-                              )));
-                },
-                child: const Text("Prop")),
-          ),
-          Card(
-            child: TextButton(
-                onPressed: () async{
-                  List<Tag> tags = [];
-                  await isar.getAllTags().then((value) {
-                    setState(() {
-                      tags = value;
-                    });
-                  });
-
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AddItemPage(
-                                addItemType: ItemType.costume,
-                            tags: tags,
-                              )));
-                },
-                child: const Text("Costume")),
-          ),
-          Card(
-            child: TextButton(
-                onPressed: () async{
-                  List<Tag> tags = [];
-                  await isar.getAllTags().then((value) {
-                    setState(() {
-                      tags = value;
-                    });
-                  });
-
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AddItemPage(
-                                addItemType: ItemType.furniture,
-                            tags: tags,
-                              )));
-                },
-                child: const Text("Furniture")),
-          ),
-          Card(
-            child: TextButton(
-                onPressed: () async{
-                  List<InventoryItem> inventoryItems = [];
-                  await isar.getAllInventoryItems().then((value) {
-                    setState(() {
-                      inventoryItems = value;
-                    });
-                  });
-
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AddTagPage(
-                            inventoryItems: inventoryItems,
-                          )));
-                },
-                child: const Text("Tag")),
-          ),
+          _addItemTypePage(
+              ItemType.prop, "Add Prop", Icons.architecture_outlined),
+          //architecture attachment backpack bakery badge blender casino category celebration checkroom coffee color design
+          _addItemTypePage(ItemType.furniture, "Add Furniture", Icons.deck),
+          //bed chair deck weekend
+          _addItemTypePage(ItemType.costume, "Add Costume", Icons.checkroom),
+          //wc  woman hail girl
+          _tagPage(),
         ],
+      ),
+    );
+  }
+
+  Widget _addItemTypePage(
+      ItemType itemType, String cardText, IconData cardIcon) {
+    IsarService isar = IsarService();
+    double screenWidth = MediaQuery.of(context).size.width;
+    return SizedBox(
+      width: screenWidth - 20.0,
+      child: GestureDetector(
+        onTap: () async {
+          List<Tag> tags = [];
+          await isar.getAllTags().then((value) {
+            setState(() {
+              tags = value;
+            });
+          });
+
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AddItemPage(
+                        addItemType: itemType,
+                        tags: tags,
+                      )));
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  cardText,
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                Icon(
+                  cardIcon,
+                  size: 30.0,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _tagPage() {
+    IsarService isar = IsarService();
+    double screenWidth = MediaQuery.of(context).size.width;
+    return SizedBox(
+      width: screenWidth - 20.0,
+      child: GestureDetector(
+        onTap: () async {
+          List<InventoryItem> inventoryItems = [];
+          await isar.getAllInventoryItems().then((value) {
+            setState(() {
+              inventoryItems = value;
+            });
+          });
+
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AddTagPage(
+                        inventoryItems: inventoryItems,
+                      )));
+        },
+        child: const Card(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Add Tag",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                Icon(
+                  Icons.discount,
+                  size: 30.0,
+                ) //discount tag
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
